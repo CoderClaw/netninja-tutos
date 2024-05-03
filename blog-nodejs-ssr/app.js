f9af0efed91e5676caf4ba83b0188cdbbbadd39f
CoderClaw <100720457+CoderClaw@users.noreply.github.com>
2024-05-03 16:04:56 -0300

    blog app + mongodb

--- blog-nodejs-ssr/app.js
+++ blog-nodejs-ssr/app.js
@@ -1,13 +1,26 @@
 require('dotenv').config();
 
 const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+
+const connectDB = require('./server/config/db.js')
 
 const app = express();
 const PORT = 5000 || process.env.PORT;
 
-app.get('', (req,res)=>{
-    res.send("hello world")
-})
+app.use(express.static('public'))
+
+//connect to db
+
+connectDB();
+
+//templating engine
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+app.use('/', require('./server/routes/main.js'));
+
 
 app.listen(PORT,()=>{
     console.log(`App listening on port ${PORT}`)
