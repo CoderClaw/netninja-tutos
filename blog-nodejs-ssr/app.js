b8d19e148acb8421743b1333e817b0880fde51a2
CoderClaw <100720457+CoderClaw@users.noreply.github.com>
2024-05-04 20:58:08 -0300

    finished project

--- blog-nodejs-ssr/app.js
+++ blog-nodejs-ssr/app.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
+const methodOverride = require('method-override')
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 
 const connectDB = require('./server/config/db.js')
+const {isActiveRoute} = require('./server/helpers/routeHelpers.js')
 
 const app = express();
 const PORT = 5000 || process.env.PORT;
@@ -20,6 +22,7 @@ connectDB();
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cookieParser())
+app.use(methodOverride('_method'))
 
 app.use(session({
     secret: "keyboard cat",
--- blog-nodejs-ssr/server/routes/admin.js
+++ blog-nodejs-ssr/server/routes/admin.js
@@ -192,4 +192,83 @@ router.post('/add-post',authMiddleware, async (req,res)=>{
     
 })
 
+/*
+    GET
+    Admin -edit post
+    
+*/
+
+router.get('/edit-post/:id',authMiddleware, async (req,res)=>{
+    try {
+
+        const locals = {
+            title: "Edit Post",
+            description: "simple blog app"
+        }  
+
+        const data = await Post.findOne({_id: req.params.id})
+
+        res.render('admin/edit-post',{
+            locals,
+            data,
+            layout: adminLayout
+        })
+    } catch (error) {
+        console.log(error)
+    }
+    
+})
+
+/*
+    Put
+    Admin -edit post
+    
+*/
+
+router.put('/edit-post/:id',authMiddleware, async (req,res)=>{
+    try {
+        await Post.findByIdAndUpdate(req.params.id,{
+            title: req.body.title,
+            body: req.body.body,
+            updatedAt: Date.now()
+        })
+        res.redirect(`/edit-post/${req.params.id}`)
+    } catch (error) {
+        console.log(error)
+    }
+    
+})
+
+/*
+    Delete
+    Admin -delete post
+    
+*/
+
+router.delete('/delete-post/:id',authMiddleware, async (req,res)=>{
+    try {
+        await Post.deleteOne({_id:req.params.id})
+        res.redirect("/dashboard")
+    } catch (error) {
+        console.log(error)
+    }
+    
+})
+
+
+/*
+    get
+    Admin -Logout
+    
+*/
+
+router.get('/logout',authMiddleware, async (req,res)=>{
+    try {
+        res.clearCookie('token')
+        res.redirect("/login")
+    } catch (error) {
+        console.log(error)
+    }
+    
+})
 module.exports = router
